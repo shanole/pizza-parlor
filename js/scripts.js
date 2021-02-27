a517391e6102b6041a728b39b620291ff3dcd3fd
shanole <73411129+shanole@users.noreply.github.com>
2021-02-26 21:12:58 -0800

    Add OrdersList object

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,39 @@
+// Business Logic for Orders List -----------
+function OrdersList() {
+  this.pizzas = {};
+  this.currentId = 0;
+}
+
+OrdersList.prototype.addPizza(function(pizza) {
+  pizza.id = this.assignId();
+  this.pizzas[pizza.id] = pizza;
+})
+
+OrdersList.prototype.assignId(function(pizza) {
+  this.currentId += 1;
+  return this.currentId;
+})
+
+OrdersList.prototype.findContact = function (id) {
+  if (this.pizzas[id] != undefined) {
+    return this.pizzas[id];
+  }
+  else {
+    return false;
+  }
+}
+
+AddressBook.prototype.deleteContact = function (id) {
+  if (this.pizzas[id] === undefined) {
+    return false;
+  }
+  else {
+    delete this.pizzas[id];
+    return true;
+  }
+}
+
+// Business Logic for pizza --------------
 small = 10;
 medium = 12;
 large = 14;
