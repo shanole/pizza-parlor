bc7cbfcd95cba83af156aab812179ed72cbc3fdd
shanole <73411129+shanole@users.noreply.github.com>
2021-02-26 22:23:50 -0800

    Add UI to make basic form for pizza order work

--- js/scripts.js
+++ js/scripts.js
@@ -4,17 +4,17 @@ function OrdersList() {
   this.currentId = 0;
 }
 
-OrdersList.prototype.addPizza(function(pizza) {
+OrdersList.prototype.addPizza = function(pizza) {
   pizza.id = this.assignId();
   this.pizzas[pizza.id] = pizza;
-})
+}
 
-OrdersList.prototype.assignId(function(pizza) {
+OrdersList.prototype.assignId = function(pizza) {
   this.currentId += 1;
   return this.currentId;
-})
+}
 
-OrdersList.prototype.findContact = function (id) {
+OrdersList.prototype.findPizza = function (id) {
   if (this.pizzas[id] != undefined) {
     return this.pizzas[id];
   }
@@ -23,7 +23,7 @@ OrdersList.prototype.findContact = function (id) {
   }
 }
 
-AddressBook.prototype.deleteContact = function (id) {
+OrdersList.prototype.deleteContact = function (id) {
   if (this.pizzas[id] === undefined) {
     return false;
   }
@@ -34,24 +34,57 @@ AddressBook.prototype.deleteContact = function (id) {
 }
 
 // Business Logic for pizza --------------
-small = 10;
-medium = 12;
-large = 14;
 
 function Pizza() {
   this.size;
   this.toppings = [];
 }
 
-Pizza.prototype.addTopping(function(topping) {
+Pizza.prototype.addTopping = function(topping) {
   this.toppings.push(topping);
-})
+}
 
-Pizza.prototype.pickSize(function(size) {
+Pizza.prototype.pickSize = function(size) {
   this.size = size;
-})
+}
 
-Pizza.prototype.getCost(function() {
+Pizza.prototype.getCost = function() {
   let basePrice = this.size*0.6;
   let toppingsPrice = (this.toppings.length()-2) * 2;
+}
+
+const pizzaSizes = {
+  "small" : 10,
+  "medium" : 12,
+  "large" : 14,
+}
+
+// User Interface Logic --------------
+let ordersList = new OrdersList();
+
+function displayPizzaDetails(ordersListToDisplay) {
+
+}
+
+function showPizza(pizzaId) {
+  const pizza = OrdersList.findPizza(pizzaId)
+}
+
+$(document).ready(function () {
+  $("form#pizza-order").submit(function (event) {
+    event.preventDefault();
+
+    pizza = new Pizza();
+
+    const pizzaSize = $("#size").val();
+    $("input:checkbox[name=toppings]:checked").each(function (){
+      const topping = $(this).val();
+      pizza.toppings.push(topping);
+    })
+
+    pizza.pickSize(pizzaSize);
+
+    console.log(pizza.size);
+    console.log(pizza.toppings);
+  })
 })
